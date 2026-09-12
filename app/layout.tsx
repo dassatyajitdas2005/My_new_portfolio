db06b6f45de4b4161402cb54b93c6fe51609a495
Satyajit Das <dassatyajitdas2005@gmail.com>
2026-09-13 01:33:43 +0530

    feat: add google-site-verification meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -71,6 +71,9 @@ export const metadata: Metadata = {
       "max-snippet": -1,
     },
   },
+  verification: {
+    google: "D66YiNdMbfkrPDnugtNmltFDlHhCHVn6oXEiWPIRDdA",
+  },
   icons: {
     icon: [
       { url: "/favicon.ico", sizes: "any" },
